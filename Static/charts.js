5325017d33669c9de12074bfe859ef4a886f46c2
domivillacis <domi.villacism@gmail.com>
2022-12-27 16:17:07 -0500

    adding js file

--- Static/charts.js
+++ Static/charts.js
@@ -164,7 +164,7 @@ function buildCharts(sample) {
     
     // Deliverable 3: 5. Create the layout for the gauge chart.
     var layout = {  
-      width: 600, height: 500, margin: { t: 0, b: 0 } 
+      width: 450, height: 450, margin: { t: 0, b: 0 } 
   };
 
     // Deliverable 3: 6. Use Plotly to plot the gauge data and layout.
